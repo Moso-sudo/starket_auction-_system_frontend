bce6bb0f4702847d89a49f6df3e4c7ce355fbfa9
Moso-sudo <mosokimothon@gmail.com>
2024-12-07 15:30:59 +0300

    front starknet using scaffold

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+import Image from "next/image";
+import faucet from "../../public/assets/faucetBanner.svg";
+import deployer from "../../public/assets/deployerBanner.svg";
+import wikipedia from "../../public/assets/wikipediaBanner.svg";
+import addressBook from "../../public/assets/addressBook.svg";
+import converter from "../../public/assets/converterBanner.svg";
+import burnerWallet from "../../public/assets/burnerWallet.svg";
+import Link from "next/link";
+import Upright from "public/svg/Upright";
+import NetworkSwitcher from "./components/lib/NetworkSwitcher";
+import Header from "./components/internal/Header";
+import AddTokenButton from "./components/lib/AddToken";
+
+export default function Home() {
+  // contractABi=""
+  // contactAddress=""0x34ba56f92265f0868c57d3fe72ecab144fc96f97954bbbc4252cef8e8a979ba
+  return (
+  
+    <main className="flex min-h-svh flex-col justify-between gap-16">
+      <Header />
+    <div className="mt-40 rounde-full items-center flex ">
+
+ <select name="" id="">  
+      <option value="">dantora moster</option>
+      <option value="">dantora moster</option>
+      <option value="">dantora moster</option>
+      <option value="">dantora moster</option>
+      <option value="">dantora moster</option>
+      <option value=""></option>
+      <option value="">moso jitu </option>
+    </select>
+
+
+    </div>   
+
+    </main>
+  );
+}
